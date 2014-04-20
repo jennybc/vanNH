476b195445e306c2e8c3bc925dca6f482a864a67
jennybc <jenny@stat.ubc.ca>
2014-04-20 00:23:12 -0700

    First draft of script to process a game

---
+++ scripts/03_process-game-data.r
@@ -0,0 +1,140 @@
+library(plyr)
+
+## when run in batch mode, provide game identifier on command line
+options <- commandArgs(trailingOnly = TRUE)
+
+if(length(options) < 1) {
+  game <- "2014-04-12_vanNH-at-pdxST"
+} else {
+  game <- options[1]
+}
+
+game_dir <- file.path("..", "games", game, "concatGoogleExtract")
+
+in_file <- file.path(game_dir, paste0(game, "_gameplay-raw.tsv"))
+game_play <- read.delim(in_file, stringsAsFactors = FALSE)
+str(game_play)
+
+in_file <- file.path(game_dir, paste0(game, "_points-raw.tsv"))
+point_info <- read.delim(in_file, stringsAsFactors = FALSE)
+str(point_info)
+
+## add short version of the teams
+mlu_teams <- read.delim(file.path("..", "data", "mlu-teams.tsv"))
+point_info$pullTeam <-
+  factor(mlu_teams$team[match(point_info$Pulling.team, mlu_teams$longName)])
+
+## replace NAs in game_play$Offense and game_play$Defense with ""
+jFun <- function(x) {x[is.na(x)] <- ""; return(x)}
+game_play <-
+  transform(game_play, Offense = jFun(Offense), Defense = jFun(Defense))
+
+## formalize this later ... I want to see NO LETTERS here!
+# table(substr(game_play$Defense, 1, 1))
+# table(substr(game_play$Defense, 1, 1))
+#       1   2   3   4   5   6   7   8   9 
+# 415  27  40  23  19   5  17  13  72   9 
+
+## separate, e.g. 81D into 81 and D
+jFun <- function(x) {
+  tmp <- regexpr("[0-9]+", x)
+  return(substring(x, tmp, tmp + attr(tmp, "match.length") - 1))
+}
+game_play <-
+  transform(game_play, oNum = I(jFun(Offense)), dNum = I(jFun(Defense)))
+
+jFun <- function(x) {
+  tmp <- regexpr("[a-zA-Z]+", x)
+  return(substring(x, tmp, tmp + attr(tmp, "match.length") - 1))
+}
+game_play <-
+  transform(game_play, oCode = I(jFun(Offense)), dCode = I(jFun(Defense)))
+
+## add an event counter within point
+game_play$event <- ddply(game_play['point'], ~ point, row)[ , 2]
+
+## TO DO: systematically populate the pull, if somehow missing
+## temporary kludge for 2014-04-12_vanNH-at-pdxST
+if(game == "2014-04-12_vanNH-at-pdxST") {
+  fix_me <- with(game_play, point == 2 & event == 1)
+  game_play[fix_me, 'Defense'] <- '?P'
+  game_play[fix_me, 'dNum'] <- '?'
+  game_play[fix_me, 'dCode'] <- 'P'
+}
+
+## add variables to hold team on O and on D
+game_play$oTeam <- game_play$dTeam <- 
+  factor(NA, levels = levels(point_info$pullTeam))
+game_play$dTeam <- rep(point_info$pullTeam,
+                       aggregate(event ~ point, game_play, max)$event)
+## populate oTeam based on dTeam
+get_opponent <- function(x) {
+  jLevels <- levels(x)
+  x <- ifelse(unclass(x) == 1, 2, 1)
+  return(jLevels[x])
+}
+game_play$oTeam <- get_opponent(game_play$dTeam)
+
+## insert a row if a D is recorded on same row as O and separate the info
+jFun <- function(x) {
+  fix_me <- which(with(x, dNum != "" & oNum != "" & dCode == "D"))
+  if(length(fix_me) > 0) {
+    x <- x[rep(1:nrow(x), ifelse(1:nrow(x) %in% fix_me, 2, 1)), ]
+    x[fix_me, c('Defense', 'dNum', 'dCode')] <- ''
+    x[fix_me + 1, c('Offense', 'oNum', 'oCode')] <- ''
+    x$event <- 1:nrow(x)    
+  }
+  return(x)
+}
+game_play <- ddply(game_play, ~ point, jFun)
+
+## tidy up
+game_play <- with(game_play,
+                  data.frame(point, event,
+                             oTeam, dTeam,
+                             oNum, oCode, dNum, dCode))
+
+## determine who's on O vs. D, at the event level
+## ... leave this for another day
+## let's just figure out who scored
+jFun <- function(x) {
+  n <- nrow(x)
+  jCodes <- c(x$oCode[n], x$dCode[n])
+  is_a_score <- 'G' %in% jCodes | 'LG' %in% jCodes
+  if(is_a_score) {
+    if(x$dNum[n] != '') {
+      scTeam <- x$dTeam[n]
+    } else {
+      scTeam <- x$oTeam[n]
+    }
+  } else {
+    scTeam <- NA
+  }
+  return(data.frame(x, scTeam))
+}
+game_play <- ddply(game_play, ~ point, jFun)
+
+out_dir <- file.path("..", "games", game, "cleanGoogleExtract")
+if(!file.exists(out_dir)) dir.create(out_dir)
+
+out_file <- file.path(out_dir, paste0(game, "_gameplay-clean.tsv"))
+write.table(game_play, out_file, quote = FALSE, sep = "\t", row.names = FALSE)
+message("wrote ", out_file)
+
+## add who scored to point_info, among other things
+point_info$scTeam <- game_play$scTeam[game_play$event == 1]
+point_info <-
+  with(point_info,
+       data.frame(point, Period, Clock.before.point, Clock.after.point,
+                  pullTeam, scTeam))
+jLevels <- levels(point_info$pullTeam)
+jFun <- function(x) {x[is.na(x)] <- FALSE; x}
+point_info$teamOne <- cumsum(jFun(with(point_info, scTeam == jLevels[1])))
+point_info$teamTwo <- cumsum(jFun(with(point_info, scTeam == jLevels[2])))
+point_info <-
+  rename(point_info, c("teamOne" = jLevels[1], "teamTwo" = jLevels[2]))
+
+out_file <- file.path(out_dir, paste0(game, "_points-clean.tsv"))
+write.table(point_info, out_file, quote = FALSE, sep = "\t", row.names = FALSE)
+message("wrote ", out_file)
+## write the abbreviated output I will use during the game
