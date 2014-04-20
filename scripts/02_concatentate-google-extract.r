dee21389e8d977845b6228934a7c1ab7a771ca6d
jennybc <jenny@stat.ubc.ca>
2014-04-19 20:02:55 -0700

    Get game identifier from command line args, for the R script this time

--- scripts/02_concatentate-google-extract.r
+++ scripts/02_concatentate-google-extract.r
@@ -1,13 +1,17 @@
 library(yaml)
 library(plyr)
 
-game <- "2014-04-12_vanNH-at-pdxST"
+options <- commandArgs(trailingOnly = TRUE)
+
+#game <- "2014-04-12_vanNH-at-pdxST"
+game <- options[1]
 
 game_dir <-
   list.files(file.path("..", "games"), pattern = game, full.names = TRUE)
 google_dir <- file.path(game_dir, "rawGoogleExtract")
 google_files <- list.files(google_dir, full.names = TRUE)
 nPoints <- length(google_files)
+message(nPoints, " point files found\n")
 
 jFun <- function(point) {
   point_string <- sprintf("%02d", point)
@@ -19,24 +23,26 @@ jFun <- function(point) {
 }
 
 rawData <- alply(seq_len(nPoints), 1, jFun)
-str(rawData, list.len = 2)
+#str(rawData, list.len = 2)
 
 game_play <- ldply(rawData, function(x) x$point_data)
 game_play <- rename(game_play, c("X1" = "point"))
 game_play$point <- as.integer(game_play$point)
-str(game_play)
+#str(game_play)
 
 point_info <- ldply(rawData, function(x) {
   data.frame(x$point_info, stringsAsFactors = FALSE)})
 point_info <- rename(point_info, c("X1" = "point"))
 point_info$point <- as.integer(point_info$point)
-str(point_info)
+#str(point_info)
 
 out_dir <- file.path("..", "games", game, "concatGoogleExtract")
 if(!file.exists(out_dir)) dir.create(out_dir)
 
 out_file <- file.path(out_dir, paste0(game, "_gameplay-raw.tsv"))
 write.table(game_play, out_file, quote = FALSE, sep = "\t", row.names = FALSE)
+message("wrote ", out_file)
 
 out_file <- file.path(out_dir, paste0(game, "_points-raw.tsv"))
 write.table(point_info, out_file, quote = FALSE, sep = "\t", row.names = FALSE)
+message("wrote ", out_file)
