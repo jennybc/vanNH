b780b44c2d909e525ec7d05f7322dc1d4bbfcdba
jennybc <jenny@stat.ubc.ca>
2014-06-10 12:37:41 -0700

    strip.white = TRUE when concatenating raw Google spreadsheet data
    Processing 2014-04-12_vanNH-at-pdxST made me aware this was needed.

--- scripts/02_concatentate-google-extract.r
+++ scripts/02_concatentate-google-extract.r
@@ -4,9 +4,9 @@ library(plyr)
 options <- commandArgs(trailingOnly = TRUE)
 
 if(length(options) < 1) {
-  #game <- "2014-04-12_vanNH-at-pdxST"
+  game <- "2014-04-12_vanNH-at-pdxST"
   #game <- "2014-04-20_sfoDF-at-vanNH"
-  game <- "foo"
+  #game <- "foo"
 } else {
   game <- options[1]
 }
@@ -17,25 +17,27 @@ google_files <- list.files(google_dir, full.names = TRUE)
 nPoints <- length(google_files)
 message(nPoints, " point files found\n")
 
+## function imports data for a point -- gameplay and point-level info
 jFun <- function(point) {
   point_string <- sprintf("%02d", point)
   point_file <- grep(paste0("point", point_string), google_files, value = TRUE)
   point_info <- yaml.load(paste(readLines(point_file, n = 4), collapse = "\n"))
-  point_data <- read.delim(point_file, skip = 4, stringsAsFactors = FALSE,
-                           strip.white = TRUE)
+  point_data <- read.delim(point_file, skip = 4,
+                           stringsAsFactors = FALSE, strip.white = TRUE)
   return(list(point_info = point_info, point_data = point_data))
 }
-
 rawData <- alply(seq_len(nPoints), 1, jFun)
-#str(rawData, list.len = 2)
+#str(rawData, list.len = 2) # List of 47
 
+## concatenate game play data across all points
 game_play <- ldply(rawData, function(x) x$point_data)
 game_play <- rename(game_play, c("X1" = "point"))
 game_play$point <- as.integer(game_play$point)
-#str(game_play)
+# str(game_play) # 'data.frame':  ? obs. of  3 variables: point, Offense, Defense
 
-point_info <- ldply(rawData, function(x) {
-  data.frame(x$point_info, stringsAsFactors = FALSE)})
+## concatenate point data across all points
+point_info <-
+  ldply(rawData, function(x) data.frame(x$point_info, stringsAsFactors = FALSE))
 point_info <- rename(point_info, c("X1" = "point"))
 point_info$point <- as.integer(point_info$point)
 #str(point_info)
