c12ac4d3ba2f94d021d94e130071f4d13d458513
jennybc <jenny@stat.ubc.ca>
2014-06-14 08:21:12 -0700

    Better names, more conventional names

--- scripts/07_resolve-game-play.r
+++ scripts/07_resolve-game-play.r
@@ -35,20 +35,23 @@ in_file <- file.path(game_dir, paste0(game, "_points-raw.tsv"))
 point_info <- read.delim(in_file, stringsAsFactors = FALSE)
 #str(point_info)
 
-## add short version of the teams and delete long version
+## replace long team names with my official short versions
 mlu_teams <- read.delim(file.path("..", "data", "mlu-teams.tsv"))
-point_info$pullTeam <-
+point_info$pull_team <-
   factor(mlu_teams$team[match(point_info$Pulling.team, mlu_teams$longName)],
          levels = jTeams)
 point_info$Pulling.team <- NULL
-point_info <- rename(point_info, c("Period" = "period"))
+## rename vars
+point_info <-
+  rename(point_info, c("Period" = "period", "Clock.before.point" = "clk_before",
+         "Clock.after.point" = "clk_after"))
 
 ## replace NAs in game_play$pullNum and game_play$recvNum with ""
 jFun <- function(x) {x[is.na(x)] <- ""; return(x)}
 game_play <-
   transform(game_play, pullNum = jFun(pullNum), recvNum = jFun(recvNum))
 
-## collapse recvCode/recvNum/pullCode/pullNum into pl_team, pl_player, pl_code
+## collapse recvCode/recvNum/pullCode/pullNum into pl_team, pl_pnum, pl_code
 jFun <- function(x) {
   point <- x$point[1]  
   double_code <- with(x, pullCode != '' & recvCode != '')
@@ -57,13 +60,12 @@ jFun <- function(x) {
     x[double_code, ]
   }
   
-  x$play_by_recvTeam <- x$pullNum == ''
-  x$pl_team <-
-    with(x, ifelse(play_by_recvTeam, "recvTeam", "pullTeam"))
-  x$pl_player <- with(x, ifelse(play_by_recvTeam, recvNum, pullNum))
-  x$pl_code <- with(x, ifelse(play_by_recvTeam, recvCode, pullCode))
+  x$play_by_recv_team <- x$pullNum == ''
+  x$pl_team <- with(x, ifelse(play_by_recv_team, "recv_team", "pull_team"))
+  x$pl_pnum <- with(x, ifelse(play_by_recv_team, recvNum, pullNum))
+  x$pl_code <- with(x, ifelse(play_by_recv_team, recvCode, pullCode))
   
-  x[c('point', 'event', 'pl_team', 'pl_player', 'pl_code')]
+  x[c('point', 'event', 'pl_team', 'pl_pnum', 'pl_code')]
 }
 game_play <- ddply(game_play, ~ point, jFun)
 
@@ -74,14 +76,14 @@ get_opponent <- function(x) {
   return(factor(jLevels[x], levels = jLevels))
 }
 
-## replace pl_team values 'pullTeam' / 'recvTeam' with, e.g. 'vanNH' / 'seaRM'
+## replace pl_team values 'pull_team' / 'recv_team' with, e.g. 'vanNH' / 'seaRM'
 jFun <- function(x) {
   point <- x$point[1]
-  pullTeam <- point_info$pullTeam[point_info$point == point]
-  recvTeam <- get_opponent(pullTeam)
+  pull_team <- point_info$pull_team[point_info$point == point]
+  recv_team <- get_opponent(pull_team)
   period <- point_info$period[point_info$point == point]
-  x$pl_team <- revalue(x$pl_team, c("pullTeam" = as.character(pullTeam),
-                                    "recvTeam" = as.character (recvTeam)))
+  x$pl_team <- revalue(x$pl_team, c("pull_team" = as.character(pull_team),
+                                    "recv_team" = as.character (recv_team)))
   x$pl_team <- factor(x$pl_team, levels = jTeams)
   return(data.frame(period = period, x))
 }
@@ -117,11 +119,11 @@ jFun <- function(x) {
 game_play <- ddply(game_play, ~ point, jFun)
 
 # x$who_off <- factor(NA, levels = jTeams)
-# x$who_off[1] <- x[1, "pullTeam"]
+# x$who_off[1] <- x[1, "pull_team"]
 # if(x[2, "recvCode"] != 'PU') {
 #   warning("point ", point, ": no pick up (PU) after the pull!")
 # }
-# x$who_off[2] <- x[2, "pullTeam"]
+# x$who_off[2] <- x[2, "pull_team"]
 
 out_dir <- file.path("..", "games", game, "07_resolvedGame")
 if(!file.exists(out_dir)) dir.create(out_dir)
@@ -130,16 +132,16 @@ out_file <- file.path(out_dir, paste0(game, "_gameplay-resolved.tsv"))
 write.table(game_play, out_file, quote = FALSE, sep = "\t", row.names = FALSE)
 message("wrote ", out_file)
 
-## new variable scorTeam records who scored
+## new variable scor_team records who scored
 just_goals <-
   subset(game_play, grepl("L*G", pl_code), select = c(point, pl_team))
-just_goals <- rename(just_goals, c("pl_team" = "scorTeam"))
+just_goals <- rename(just_goals, c("pl_team" = "scor_team"))
 point_info <- join(point_info, just_goals)
 
 ## new variables for individual team scores
 jFun <- function(x) {x[is.na(x)] <- FALSE; x}
-point_info$teamOne <- cumsum(jFun(with(point_info, scorTeam == jTeams[1])))
-point_info$teamTwo <- cumsum(jFun(with(point_info, scorTeam == jTeams[2])))
+point_info$teamOne <- cumsum(jFun(with(point_info, scor_team == jTeams[1])))
+point_info$teamTwo <- cumsum(jFun(with(point_info, scor_team == jTeams[2])))
 point_info <-
   rename(point_info, c("teamOne" = jTeams[1], "teamTwo" = jTeams[2]))
 
@@ -147,10 +149,11 @@ out_file <- file.path(out_dir, paste0(game, "_points-resolved.tsv"))
 write.table(point_info, out_file, quote = FALSE, sep = "\t", row.names = FALSE)
 message("wrote ", out_file)
 
+# write most recent (usually the final) score to yaml for use in, e.g., index
 score_yaml <-
   file.path("..", "games", game, paste0(game, "_at-last-point.yaml"))
 ## workaround because as.yaml() segfaults if a factor contains an NA
-## scorTeam is a factor and will be NA for any point that ends due to time
+## scor_team is a factor and will be NA for any point that ends due to time
 yaml_fodder <- as.list(point_info[nrow(point_info), ])
 yaml_fodder <- lapply(yaml_fodder, function(x) {
   if(is.factor(x) & any(is.na(x))) {
