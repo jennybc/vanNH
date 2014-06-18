970e5da6fe9c43a08d3fa875ed1b0c39f3606cd2
jennybc <jenny@stat.ubc.ca>
2014-06-18 14:10:54 -0700

    Many more plots re: how possessions end

--- scripts/11_plot-possessions.r
+++ scripts/11_plot-possessions.r
@@ -1,6 +1,9 @@
 library(plyr)
 library(ggplot2)
 
+jWidth <- 5
+jHeight <- 4
+
 games <- c("2014-04-12_vanNH-at-pdxST", "2014-04-20_sfoDF-at-vanNH",
            "2014-04-26_vanNH-at-seaRM", "2014-05-10_seaRM-at-vanNH",
            "2014-05-17_vanNH-at-sfoDF", "2014-05-24_pdxST-at-vanNH",
@@ -64,78 +67,151 @@ poss_dat <- ddply(gpDat, ~ game + poss_abs, function(x) {
   n <- nrow(x)
   score <- which(grepl("L*G", x$pl_code))
   scor_team <- as.character(if(any(score)) x$pl_team[max(score)] else NA)
-  data.frame(x[n, ], pull_team, score = any(score), scor_team)
+  who <- ifelse(x$poss_team[n] == pull_team, "d_line", "o_line")
+  if(x$pl_code[n] == 'F') {
+    x$pl_code[n] <- if(who == 'o_line') "off F" else "TA"
+  }
+  data.frame(x[n, ], pull_team, score = any(score), scor_team, who)
 })
-str(poss_dat) # 842 obs. of  13 variables:
+str(poss_dat) # 842 obs. of  14 variables:
 
 ## sanity checks of poss_dat
 ddply(poss_dat, ~ game + scor_team, summarize, score = sum(score))
 ## yes agrees with actual final scores
+table(poss_dat$who)
+# o_line d_line 
+#    397    445 
+addmargins(with(poss_dat, table(who, score)))
+#      score
+# who      FALSE TRUE Sum
+#   o_line   219  178 397
+#   d_line   261  184 445
+#   Sum      480  362 842
 
-## create a new version of the pl_code that is coarser
-# d_codes <- c('D', 'HB', 'FB')
-# goal_codes <- c('G', 'LG')
-# throwaway_codes <- c('*', 'PU', 'L')
-poss_dat$a_code <-
+## reorder and revalue pl_code in poss_dat
+poss_dat$pl_code <-
   mapvalues(poss_dat$pl_code, # revalue() won't work due to factor level ''
-            from = c('D', 'HB', 'FB', 'G', 'LG',   '', 'PU',  'L'),
-            to   = c('D',  'D',  'D', 'G',  'G', 'TA', 'TA', 'TA'))
-poss_dat$a_code <- with(poss_dat, reorder(a_code, a_code, length))
-as.data.frame(table(poss_dat$a_code, dnn = "a_code"))
-
-## reorder and revalue pl_code itself
-poss_dat$pl_code <- mapvalues(poss_dat$pl_code, from = '', to = '*')
+            from = c(  '', 'PU',  'L'),
+            to   = c('TA', 'TA', 'TA'))
 poss_dat$pl_code <- with(poss_dat, reorder(pl_code, pl_code, length))
 as.data.frame(table(poss_dat$pl_code, dnn = "a_code"))
 
-## how do possessions end? naive approach based only on last pl_code, which
-## actually works quite well
-last_code_freq <- as.data.frame(table(poss_dat$a_code, dnn = "a_code"))
-last_code_freq <-
-  rbind(last_code_freq,
-        data.frame(a_code = "Sum", Freq = sum(last_code_freq$Freq)))
-last_code_freq <-
-  mutate(last_code_freq, prop = Freq / Freq[nrow(last_code_freq)],
-         pretty_prop = as.character(round(prop, 2)))
+## create a new version of the pl_code that is coarser: a_code
+poss_dat$a_code <-
+  mapvalues(poss_dat$pl_code,
+            from = c('D', 'HB', 'FB', 'G', 'LG'),
+            to   = c('D',  'D',  'D', 'G',  'G'))
+poss_dat$a_code <- with(poss_dat, reorder(a_code, a_code, length))
+as.data.frame(table(poss_dat$a_code, dnn = "a_code"))
 
-## bar chart these frequencies
-p <- ggplot(subset(last_code_freq, a_code != "Sum"), aes(x = a_code, y = prop))
-p + geom_bar(stat = "identity") +
-  geom_text(aes(label = pretty_prop), vjust = -0.2, size = 4) + 
-  xlab("how a possession ends") + ylab("proportion of possessions")
-ggsave("../web/figs/barchart_how_possessions_end.png")
+## create a new version of the a_code that is YET coarser: b_code
+poss_dat$b_code <-
+  mapvalues(poss_dat$a_code,
+            from = c(    'D',   'TA',    'TD',   'VTT',   'VST', 'off F'),
+            to   = c('def +','off -', 'off -', 'off -', 'off -', 'off -'))
+poss_dat$b_code <- with(poss_dat, reorder(b_code, b_code, length))
+as.data.frame(table(poss_dat$b_code, dnn = "b_code"))
+
+## function that counts how often a code occurs, computes a proportion, and also
+## a "pretty" proportion suitable for putting on a figure
+count_em_up <- function(code_var, x = poss_dat) {
+  code_freq <- as.data.frame(table(x[[code_var]], dnn = code_var))
+  row_to_append <- setNames(data.frame("Sum", sum(code_freq$Freq)),
+                            c(code_var, "Freq"))
+  code_freq <- rbind(code_freq, row_to_append)
+  code_freq <-
+    mutate(code_freq, prop = Freq / Freq[nrow(code_freq)],
+           pretty_prop = as.character(round(prop, 2)))
+  return(code_freq)
+}
+
+## function that assembles the common part of the "how possessions end?" series
+## of barcharts
+construct_basic_barchart <- function(code_freq, code_var, fill_var = NULL) {
+  non_sum_rows <- code_freq[[code_var]] != "Sum"
+  if(is.null(fill_var)) {
+    p <- ggplot(subset(code_freq, non_sum_rows),
+                aes_string(x = code_var, y = "prop")) +
+      geom_bar(stat = "identity")
+  } else {
+    p <- ggplot(subset(code_freq, non_sum_rows),
+                aes_string(x = code_var, y = "prop", fill = fill_var)) +
+      geom_bar(stat = "identity", position = "dodge")
+  }
+  p + xlab("how possessions end") + ylab("proportion of possessions")
+}
+
+## how do possessions end? using very coarse b_code
+last_code_freq <- count_em_up("b_code")
+p <- construct_basic_barchart(last_code_freq, "b_code")
+p + geom_text(aes(label = pretty_prop), vjust = -0.2, size = 4)
+ggsave("../web/figs/barchart_how_possessions_end_coarse.png",
+       width = jWidth, height = jHeight)
+
+## how do possessions end? using b_code AND O line vs D line
+last_code_freq_by_line <-
+  ddply(poss_dat, ~ who, function(x) count_em_up("b_code", x))
+p <- construct_basic_barchart(last_code_freq_by_line, "b_code", "who")
+p + geom_text(aes(label = pretty_prop), vjust = -0.2, size = 4,
+              position = position_dodge(0.9)) + 
+  labs(fill = "who's on offense?")
+ggsave("../web/figs/barchart_how_possessions_end_coarse_by_line.png",
+       width = jWidth, height = jHeight)
 
 ## how do possessions end? split out by poss_team
-last_code_freq_by_team <- ddply(poss_dat, ~ poss_team, function(x) {
-  tmp <- as.data.frame(table(x$a_code, dnn = "a_code"))
-  tmp <- rbind(tmp, data.frame(a_code = "Sum", Freq = sum(tmp$Freq)))
-  tmp <- mutate(tmp, prop = Freq / Freq[nrow(tmp)],
-                pretty_prop = as.character(round(prop, 2)))
-  return(tmp)  
-})
+last_code_freq_by_team <-
+  ddply(poss_dat, ~ poss_team, function(x) count_em_up("b_code", x))
+p <- construct_basic_barchart(last_code_freq_by_team, "b_code")
+p + facet_wrap(~ poss_team) + ylim(0, 0.54) +
+  geom_text(aes(label = pretty_prop), vjust = -0.2, size = 4)
+ggsave("../web/figs/barchart_how_possessions_end_coarse_by_team.png",
+       width = jWidth, height = jHeight)
 
-p <- ggplot(subset(last_code_freq_by_team, a_code != "Sum"),
-            aes(x = a_code, y = prop))
-p + geom_bar(stat = "identity") + facet_wrap(~ poss_team) +
-  geom_text(aes(label = pretty_prop), vjust = -0.2, size = 4) + 
-  xlab("how a possession ends") + ylab("proportion of possessions")
-ggsave("../web/figs/barchart_how_possessions_end_by_team.png")
+## how do possessions end? using b_code AND O line vs D line AND poss_team
+last_code_freq_by_line_and_team <-
+  ddply(poss_dat, ~ poss_team + who, function(x) count_em_up("b_code", x))
+p <- construct_basic_barchart(last_code_freq_by_line_and_team, "b_code", "who")
+p + facet_wrap(~ poss_team) + 
+  geom_text(aes(label = pretty_prop), vjust = -0.2, size = 2,
+            position = position_dodge(0.9)) + 
+  labs(fill = "who's on offense?") + ylim(0, 0.6)
+ggsave("../web/figs/barchart_how_possessions_end_coarse_by_line_and_team.png",
+       width = jWidth, height = jHeight)
 
+## how do possessions end? using a_code
+last_code_freq <- count_em_up("a_code")
+p <- construct_basic_barchart(last_code_freq, "a_code")
+p + geom_text(aes(label = pretty_prop), vjust = -0.2, size = 4)
+ggsave("../web/figs/barchart_how_possessions_end_detailed.png",
+       width = jWidth, height = jHeight)
 
+## how do possessions end? using a_code AND O line vs D line
+last_code_freq_by_line <-
+  ddply(poss_dat, ~ who, function(x) count_em_up("a_code", x))
+p <- construct_basic_barchart(last_code_freq_by_line, "a_code", "who")
+p + geom_text(aes(label = pretty_prop), vjust = -0.2, size = 2,
+              position = position_dodge(0.9)) + 
+  labs(fill = "who's on offense?")
+ggsave("../web/figs/barchart_how_possessions_end_detailed_by_line.png",
+       width = jWidth, height = jHeight)
 
-d_ply(poss_dat, ~ pl_code, function(x) {
-  match_vars <- c('game', 'period', 'point', 'event')
-  gp_rows <-
-    join(x[match_vars],
-         data.frame(gpDat[match_vars], row = seq_len(nrow(gpDat))))$row
-  display_vars <- c('game', 'point', 'event', 'poss_team', 'pl_team',
-                    'pl_pnum', 'pl_code')
-  for(i in seq_along(gp_rows)) {
-    print(gpDat[gp_rows[i] + (-2:2), display_vars])
-    cat("\n")
-  }
-  
-})
+## how do possessions end? split out by poss_team
+last_code_freq_by_team <-
+  ddply(poss_dat, ~ poss_team, function(x) count_em_up("a_code", x))
+p <- construct_basic_barchart(last_code_freq_by_team, "a_code")
+p + facet_wrap(~ poss_team) + ylim(0, 0.54) +
+  geom_text(aes(label = pretty_prop), vjust = -0.2, size = 4)
+ggsave("../web/figs/barchart_how_possessions_end_detailed_by_team.png")
+
+## how do possessions end? split out by o_line vs d_line and team
+last_code_freq_by_line_and_team <-
+  ddply(poss_dat, ~ poss_team + who, function(x) count_em_up("a_code", x))
+p <- construct_basic_barchart(last_code_freq_by_line_and_team, "a_code", "who")
+p + facet_wrap(~ poss_team) + 
+  geom_text(aes(label = pretty_prop), vjust = -0.2, size = 2,
+            position = position_dodge(0.9)) + 
+  labs(fill = "who's on offense?") + ylim(0, 0.6)
+ggsave("../web/figs/barchart_how_possessions_end_detailed_by_line_and_team.png")
 
 
 ## aggregate to points: record how many possessions, who scored (if anyone),
@@ -243,3 +319,22 @@ p + geom_bar(stat = "identity") +
   theme(legend.position = c(1, 1), legend.justification = c(1, 1),
         legend.background = element_rect(fill = 0)) + labs(fill = "")
 ggsave("../web/figs/poss_n_dist_by_scor_team_and_status.png")
+
+
+## NOT IN USE
+## for viewing rows in gpDat where something happens, e.g. a possession ends
+## with a specific code
+# d_ply(poss_dat, ~ pl_code, function(x) {
+#   match_vars <- c('game', 'period', 'point', 'event')
+#   gp_rows <-
+#     join(x[match_vars],
+#          data.frame(gpDat[match_vars], row = seq_len(nrow(gpDat))))$row
+#   display_vars <- c('game', 'point', 'event', 'poss_team', 'pl_team',
+#                     'pl_pnum', 'pl_code')
+#   for(i in seq_along(gp_rows)) {
+#     print(gpDat[gp_rows[i] + (-2:2), display_vars])
+#     cat("\n")
+#   }
+#   
+# })
+
