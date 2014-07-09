09ef3512542c64bcc875eb69261e7e4e0c6c75ac
jennybc <jenny@stat.ubc.ca>
2014-07-08 22:21:09 -0700

    Tidying Make targets and re-cleaning, checking, resolving.

--- scripts/33_omg-I-should-be-better-at-Make.r
+++ scripts/33_omg-I-should-be-better-at-Make.r
@@ -7,6 +7,7 @@ game_info <- game_info[match(keeper_games, game_info$game), ]
 
 #make_pre <- 'make r_bits'
 #make_pre <- 'make clean_game'
+#make_pre <- 'make clean_clean'
 #make_pre <- 'make check_clean'
 #make_pre <- 'make clean_resolve'
 make_pre <- 'make resolve_game'
