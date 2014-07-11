81e18a8dc05fe1aa9b5f5f87f9af186ede254e5d
jennybc <jenny@stat.ubc.ca>
2014-07-11 13:36:38 -0700

    Early run of resolving passes for all games at once

---
+++ scripts/07_resolve-passes.r
@@ -0,0 +1,78 @@
+library(plyr)
+
+## when run in batch mode, provide game identifier on command line
+options <- commandArgs(trailingOnly = TRUE)
+
+if(length(options) < 1) {
+  #game <- "2014-04-12_vanNH-at-pdxST"
+  #game <- "2014-04-20_sfoDF-at-vanNH"
+  #game <- "2014-04-26_vanNH-at-seaRM"
+  #game <- "2014-05-10_seaRM-at-vanNH"
+  #game <- "2014-05-17_vanNH-at-sfoDF"
+  #game <- "2014-05-24_pdxST-at-vanNH"
+  #game <- "2014-05-31_vanNH-at-seaRM"
+  #game <- "2014-06-07_seaRM-at-vanNH"
+  #game <- "2014-06-15_pdxST-at-vanNH"
+  game <- "2014-05-04_sfoDF-at-seaRM"
+} else {
+  game <- options[1]
+}
+
+## parse the game identifier
+tmp <- strsplit(game, split = "_")[[1]]
+game_date <- tmp[1]
+tmp <- strsplit(tmp[2], split = "-")[[1]]
+away_team <- tmp[1]
+home_team <- tmp[3]
+jTeams <- sort(c(away_team, home_team))
+
+game_dir <- file.path("..", "games", game, "06_possess-game")
+in_file <- file.path(game_dir, paste0(game, "_gameplay-resolved.tsv"))
+game_play <- read.delim(in_file, stringsAsFactors = FALSE,
+                        ## do this because of seaRM-rupp-00
+                        colClasses = list(pl_pnum = "character"))
+
+#str(game_play)
+message(game, ":\n  ", nrow(game_play), " rows of resolved game play found")
+
+game_play <-
+  mutate(game_play,
+         who = ifelse(poss_team == pl_team, 'O', 'D'),
+         pl_code = mapvalues(pl_code, # no joy w/ revalue() due to level ''
+                             from = '', to = '*'))
+j_revalues <- c("OBP" = "P", "*" = "CTH", "A" = "CTH",
+                "LG" = "G", "L" = "CTH", "PUA" = "PU",
+                "LA" = "CTH", "HB" = "D", "FB" = "D",
+                "VTT" = "OV", "VST" = "OV")
+game_play <-
+  mutate(game_play,
+         pl_code = revalue(pl_code, j_revalues, warn_missing = FALSE))
+
+source("50_pass-resolve-script.r")
+#x <- subset(game_play, point == 1 & poss_rel == 2)
+#resolve_passes(x)
+pass_dat <- ddply(game_play, ~ poss_abs + poss_rel + point, resolve_passes)
+#str(pass_dat, max.level = 1)
+
+out_dir <- file.path("..", "games", game, "07_pass-game")
+if(!file.exists(out_dir)) dir.create(out_dir)
+
+message("  ", nrow(pass_dat), " rows of resolved pass data will be written\n")
+
+out_file <- file.path(out_dir, paste0(game, "_passes.tsv"))
+write.table(pass_dat, out_file, quote = FALSE, sep = "\t", row.names = FALSE)
+#message("wrote ", out_file)
+
+#with(pass_dat, table(desc, n_inn))
+out_file <- file.path(out_dir, paste0(game, "_passes-tally.tsv"))
+write.table(as.data.frame(with(pass_dat, table(desc, n_inn))), out_file,
+            quote = FALSE, sep = "\t", row.names = FALSE)
+
+# head(foo)
+# 
+# with(subset(foo, desc == "ao" & n_inn == 1),
+#      table(innards))
+# 
+# subset(foo, desc == "an")
+#      table(innards))
+
--- scripts/50_pass-resolve-script.r
+++ scripts/50_pass-resolve-script.r
@@ -1,4 +1,32 @@
 resolve_passes <- function(x) {
+  ## detect data with more than one possession indicated by poss_abs | poss_rel
+  if(!is.null(x$poss_abs)) {
+    poss <- "poss_abs"
+  } else if (!is.null(x$poss_rel)) {
+    poss <- "poss_rel"
+  } else {
+    poss <- NULL
+  }
+  if(!is.null(poss)) {
+    if(length(unique(x[[poss]])) > 1) {
+      message("  ALERT point ", x$point[1], " has more than one possession")
+      message("  resolving passes only for the last possession provided")
+      x <- x[x[[poss]] == max(x[[poss]]), ]
+    }
+  }
+  ## detect data with more than one possession indicated via poss_team
+  if(length(unique(x$poss_team)) > 1) {
+    message("  ALERT detected >1 poss_team when resolving passes for ",
+            "a possession in point ", x$point[1])
+    message("  no passes will be returned for the affected possession")
+    return(NULL)
+  }
+  
+  require(plyr)
+  x <- mutate(x,
+              is_alpha = pl_code %in% c("CTH", "PU"),
+              is_omega = pl_code %in% c("CTH", "D", "G", "TD", "OV"),
+              e_code = paste(who, pl_code, sep = "-"))
   n <- nrow(x)
   ## trim off the top: pulls, subs out or in, timeouts
   alpha_row <- min(which(!(x$pl_code %in% c('P', 'SO', 'SI', 'TO'))))
@@ -16,6 +44,10 @@ resolve_passes <- function(x) {
   i <- 1
   while(i <= n) {
     if(y$is_alpha[i]) {
+      ##I AM STEPPING THROUGH THIS
+      ## FIND OUT WHAT HAPPENS WHEN POSSESSION ENDS
+      ## IT SHOULD NEVER HAPPEN BUT IF I FEED STUPID DATA IT WILL
+      ## STUDY BEHAVIOR, THEN BUILD IN A STOP FOR THIS
       suppressWarnings(next_omega <- min(which(seq_len(n) > i & y$is_omega)))
       suppressWarnings(next_alpha <- min(which(seq_len(n) > i & y$is_alpha)))
       desc <- switch(which.min(c(next_omega, next_alpha, n)), "ao", "aa", "an")
@@ -26,14 +58,16 @@ resolve_passes <- function(x) {
       tmp[[i]] <-
         with(y,
              data.frame(beg_event = event[i], end_event = event[end_row],
-                desc = desc, n_inn = end_row - i - 1, 
-                beg_code = y$e_code[i], beg_pnum = pl_pnum[i],
-                innards = innards,
-                end_code =if(desc == "an")NA_character_ else y$e_code[end_row],
-                end_pnum = pl_pnum[end_row]))
+                        desc = desc, n_inn = end_row - i - 1, 
+                        beg_code = e_code[i], #beg_pnum = I(pl_pnum[i]),
+                        beg_plyr = I(paste(poss_team[i], pl_pnum[i], sep = "-")),
+                        innards = I(innards),
+                        end_code =if(desc == "an")NA_character_ else e_code[end_row],
+                        #end_pnum = I(pl_pnum[end_row]),
+                        end_plyr = I(paste(poss_team[end_row], pl_pnum[end_row], sep = "-"))))
       if(i == end_row) {
         break
-        } else {
+      } else {
           i <- end_row
           next
         }
@@ -44,5 +78,5 @@ resolve_passes <- function(x) {
       }
     }
   }
-  return(data.frame(do.call("rbind", tmp)))
+  return(do.call("rbind", tmp))
 }
